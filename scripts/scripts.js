9e2a211862274aec98161027930f912992fc8f48
Ubuntu <ubuntu@ip-172-31-89-160.ec2.internal>
2019-10-18 18:54:00 +0000

    frontface images added

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+//array with card images
+var cards = [];
+cards[0] = '../images/dancers.jpg';
+cards[1] = '../images/doodle.png';
+cards[2] = '../images/flowers.png';
+cards[3] = '../images/moon.jpg';
+cards[4] = '../images/ring.png';
+cards[5] = '../images/sketch.jpg';
+cards[6] = '../images/space.jpg';
+cards[7] = '../images/watercolor.jpg';
+cards[8] = '../images/dancers.jpg';
+cards[9] = '../images/doodle.png';
+cards[10] = '../images/flowers.png';
+cards[11] = '../images/moon.jpg';
+cards[12] = '../images/ring.png';
+cards[13] = '../images/sketch.jpg';
+cards[14] = '../images/space.jpg';
+cards[15] = '../images/watercolor.jpg';
